4d51cce5f031875a44764e005faba7d3c2b7182a
Uncle-Fox <Nemogemut@gmail.com>
2023-09-17 11:28:52 +0300

    сделали папку с примерами для работы в группе

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Написать программу, которая на вход принимает число и выдает его квадрат
 Console.Write("Введите число, которое умножится на себя ");
 int MyNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write(MyNumber * MyNumber);
+int square = MyNumber * MyNumber;
+Console.WriteLine(MyNumber + " -> " + square);
+Console.WriteLine($"{MyNumber} -> {square}");
 
